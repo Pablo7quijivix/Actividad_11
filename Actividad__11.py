b97275a682811690c6d1f84262fc3b08fbf8fc1b
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-31 09:51:12 -0600

    4 commit avances en clse

--- Actividad__11.py
+++ Actividad__11.py
@@ -2,7 +2,7 @@ propietarios ={}
 cantidad_propietarios = int(input("===¿Cuantos propietarios desea ingresar?: "))
 for i in range (cantidad_propietarios):
     print(f"Ingrese Propietario {i+1} ")
-    nit = int(input("=== INGRESE NIT ===: "))
+    nit = input("=== INGRESE NIT ===: ")
     name = input("=== INGRESE NOMBRE COMPLETO ===: ")
     telefono_contacto =int(input("=== INGRESE NUMERO DE TELEFONO ===: "))
     cantidad_vehiculos_posee = int(input("=== INGRESE CANTIDAD DE VEHICULOS QUE POSEE===: "))
@@ -14,18 +14,37 @@ for i in range (cantidad_propietarios):
         marca = input("INGRESE LA MARCA DEL CARRO: ")
         model = input(f"INGRESE  EL MODELO DE SU CARRO => {marca} <= \n(===EJEMPLO===: TOYOTA: COROLLA, FORD: RAPTOR, NISSAN:ROGUE ETC..... ): ")
         year = int(input("INGRES AÑO DEL CARRO: "))
-        estado_impuesto = input("INGRESE ESTADO DEL IMPUESTO(SI O NO): ")
+        estado_impuesto = input("¿PAGO EL IMPUESTO? (SI O NO): ")
 
 
     propietarios[nit] = {
         "name" : name,
         "telefono_contacto" : telefono_contacto,
         "numero_carros" :{
-            "placa" : placa,
-            "marca" : marca,
-            "model" : model,
-            "year"  : year,
-            "estado_impuesto" : estado_impuesto
+            'placa' : placa,
+            'marca' : marca,
+            'model' : model,
+            'year'  : year,
+            'estado_impuesto' : estado_impuesto
         }
 
     }
+print(f"\n=====LISTADO DE PROPIETARIOS REGISTRADOS=====")
+for nit, data in propietarios.items():
+    for i in data:
+        print(f"\n===NIT===: {nit}")
+        print(f"===NOMBRE===: {name}")
+        print(f"===TELEFONO DE CONTACTO: {telefono_contacto}")
+        print(f"\n______Vehiculos :______")
+        print(f"___PLACA: {nit['placa']}")
+        print(f"___MARCA: {nit['marca']}")
+        print(f"___MODELO: {nit['model']}")
+        print(f"___AÑO: {nit['yaer']}")
+        print(f"___IMPUESTO: {nit['estado_impuesto']}")
+
+print(f"\n=======Busqueda de propietario por identificacion========")
+buscado = input("Ingrese su identificacion (NIT): ")
+if buscado in propietarios:
+    propietario = propietarios[buscado]
+    print(f"\n_____PROPIETARIO ENCONTRADO_____")
+    print(f"")
